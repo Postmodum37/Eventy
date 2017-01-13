7bd984c0023deb306d3c1e12305de9f34c856f26
Tomas Novikovas <tom.novikovas@gmail.com>
2017-01-13 12:23:32 +0200

    fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    @recent_events = Event.last(4)
-    @todays_events = Event.last(4)
-    @last_events = Event.last(4)
+    @recent_events = Event.where(start_date: (Time.zone.now - 6.hours)..Time.zone.now)
+    @todays_events = Event.where('start_date >= ? AND end_date <= ?', Time.zone.now.beginning_of_day, Time.zone.now.end_of_day)
+    @last_events = Event.order(created_at: :asc).last(4)
   end
 end
